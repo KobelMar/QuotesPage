145503edcfb0a85a913430788a8f3be764e306ec
KobelMar <max.marcisiak@gmail.com>
2022-12-23 09:04:54 +0100

    .

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,32 +1,38 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import useHttp from "../hooks/use-http";
-import {getAllQuotes} from "../lib/api"
+import { getAllQuotes } from "../lib/api";
 
 import QuoteList from "../components/quotes/QuoteList";
 import LoadingSpinner from "../components/ui/LoadingSpinner";
 import NoQuotesFounds from "../components/quotes/NoQuotesFound";
 
-
 export default function AllQuotes() {
- const {sendRequest, status, data: loadedQuotes, error} = useHttp(getAllQuotes, true)
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
 
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
 
- useEffect(()=> {
-  sendRequest()
-}, [sendRequest])
+  if (status === "pending") {
+    return (
+      <div>
+        <LoadingSpinner />
+      </div>
+    );
+  }
 
-if (status === "pending") {
-  return <div><LoadingSpinner /></div>
-}
+  if (error) {
+    return <p>{error}</p>;
+  }
 
-if (error) {
-  return <p>{error}</p>
-}
-
-if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
-  return <NoQuotesFounds />
-}
-
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFounds />;
+  }
 
   return <QuoteList quotes={loadedQuotes} />;
 }
